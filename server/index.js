b4a2e15f2f9a6927c40ba83de2445acf70988105
devFlashcap <dev.flashcap@gmail.com>
2022-02-12 02:54:09 +0100

    dotenv.config() refactoring

--- server/index.js
+++ server/index.js
@@ -1,14 +1,17 @@
 const express = require('express');
 const cors = require('cors');
 
+require('dotenv').config();
 require('./mongo.connect')();
 
 const app = express();
-const port = process.env.PORT | 5000;
+const port = process.env.EXPRESS_PORT | 2000;
 
+// Apply middleware
 app.use(cors());
 app.use(express.json());
 
+// Start server
 app.listen(port, () => {
     console.log(`Server is running on port ${port}`);
 });
